3cba2e57c474316b0437bc8982420b92b82da7d2
joakimstolen <joakimstoelen@hotmail.com>
2020-09-15 15:33:06 +0200

    added travis 4

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag docker_test --build-arg JAR_FILE=./target/Forelesning4-0.0.1-SNAPSHOT.jar
+docker build . --tag docker_test --build-arg JAR_FILE=./target/Docker-0.0.1-SNAPSHOT.jar
 docker tag docker_test  joakimstolen/docker_test
 docker push joakimstolen/docker_test
 
