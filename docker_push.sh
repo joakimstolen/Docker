34b02e05473f8e22a13ffcf7e28722b316b0832e
joakimstolen <joakimstoelen@hotmail.com>
2020-09-15 15:30:37 +0200

    added travis 3

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag forelesning4_devops --build-arg JAR_FILE=./target/Forelesning4-0.0.1-SNAPSHOT.jar
-docker tag forelesning4_devops  joakimstolen/forelesning4_devops
-docker push joakimstolen/forelesning4_devops
+docker build . --tag docker_test --build-arg JAR_FILE=./target/Forelesning4-0.0.1-SNAPSHOT.jar
+docker tag docker_test  joakimstolen/docker_test
+docker push joakimstolen/docker_test
 
