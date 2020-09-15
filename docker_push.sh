752194d261272b95d1138a4c85e9d82890190f39
joakimstolen <joakimstoelen@hotmail.com>
2020-09-15 15:53:52 +0200

    added travis 8

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build . --tag docker_test --build-arg JAR_FILE=./target/Docker-0.0.1-SNAPSHOT.jar
-docker tag docker_test  joakimstolen/docker_test
-docker push joakimstolen/docker_test
-
+docker build . --tag docker_test2 --build-arg JAR_FILE=./target/Docker-0.0.1-SNAPSHOT.jar
+docker tag docker_test2  joakimstolen/docker_test2
+docker push joakimstolen/docker_test2
