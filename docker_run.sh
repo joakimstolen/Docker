7053d42ad1e04d755c442a0191181691902525dd
joakimstolen <joakimstoelen@hotmail.com>
2020-09-17 11:39:06 +0200

    travis test 6

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag test_for_docker --build-arg JAR_FILE=./target/Docker-0.0.1-SNAPSHOT.jar
 docker tag test_for_docker joakimstolen/test_for_docker
 docker push joakimstolen/test_for_docker
